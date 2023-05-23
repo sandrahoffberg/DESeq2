dd37f7854c1b064257416572a587c534dff1de37
Sandra Hoffberg <sandra@codeocean.com>
2023-05-23 22:23:54 +0000

    Edited deseq2.R

--- code/deseq2.R
+++ code/deseq2.R
@@ -43,7 +43,7 @@ for (condition in sort(unique(metadata$TreatmentDosageTreatTime))) {
 }
 
 } else {
-    dummy_file<-file("../results/not_run.txt")
+    dummy_file<-file(paste("../results/",base_name,"_not_run.txt", sep=""))
     writeLines("DESeq2 capsule was not run", dummy_file)
     close(dummy_file)
 }
